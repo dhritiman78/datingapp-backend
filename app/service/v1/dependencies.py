ac3cf4297f89b77ce86a730d578a00b24b2bd08a
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2025-07-06 21:05:30 +0530

    fix: improved speed of token verification

--- app/service/v1/dependencies.py
+++ app/service/v1/dependencies.py
@@ -1,9 +1,13 @@
+import json
+
 from fastapi import Header, HTTPException
 from firebase_admin import auth
 from typing import Optional
 
 # 👇 Ensure Firebase is initialized
 import app.utils.firebase
+from app.database.redis import redis_client
+
 
 async def verify_token(authorization: Optional[str] = Header(None)):
     if not authorization or not authorization.startswith("Bearer "):
@@ -11,18 +15,24 @@ async def verify_token(authorization: Optional[str] = Header(None)):
 
     token = authorization.split(" ")[1]
 
+    # Try Redis
+    cached_user = await redis_client.get(f"token_{token}")
+    if cached_user:
+        return json.loads(cached_user)
+
     try:
         decoded_token = auth.verify_id_token(token)
     except Exception as e:
         raise HTTPException(status_code=401, detail=f"Invalid or expired token: {str(e)}")
 
-    return {
+    user_data = {
         "uid": decoded_token.get("uid"),
         "email": decoded_token.get("email"),
         "name": decoded_token.get("name", ""),
     }
-    # return {
-    #     "uid": "Hn96nz6u8yRabmYAJj9dhot04gk1",
-    #     "email": "psydriteofficial@gmail.com",
-    #     "name": "PsydriteStudios",
-    # }
+
+    ttl = decoded_token.get("exp", 0) - decoded_token.get("iat", 0)
+    if ttl > 0:
+        await redis_client.set(f"token_{token}", json.dumps(user_data), ex=ttl)
+
+    return user_data
