a948c31712607285747252643fdf6761c51b11d6
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2025-08-07 00:50:02 +0530

    feat: user posts

--- app/controllers/v1/posts_controller.py
+++ app/controllers/v1/posts_controller.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from typing import Optional
 
 from fastapi import UploadFile
 
 from app.service.v1.posts_service import upload_posts_service, get_posts_service
+from app.service.v1.relations_service import get_user_relations_service
 from app.utils.compress_image import compress_image
 
 
@@ -14,5 +16,11 @@ async def upload_posts_controllers (user_uid: str, picture: UploadFile, caption:
 
         return await upload_posts_service(user_uid,filename,caption,compressed_buffer)
 
-async def get_user_posts_controllers (user_uid: str):
-        return await get_posts_service(user_uid)
+async def get_user_posts_controllers(user_uid: str, target_uid: Optional[str]):
+    if target_uid:
+        relations = await get_user_relations_service(user_uid, target_uid)
+        if relations.get("is_blocked"):
+            return []
+        return await get_posts_service(target_uid)
+
+    return await get_posts_service(user_uid)
--- app/routes/v1/posts_route.py
+++ app/routes/v1/posts_route.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from fastapi import APIRouter, UploadFile, File, Form, status, Depends
 
 from app.controllers.v1.posts_controller import upload_posts_controllers, get_user_posts_controllers
+from app.models.v1.posts_model import GetUserPosts
 from app.service.v1.dependencies import verify_token
 
 router = APIRouter()
@@ -15,6 +18,7 @@ async def upload_posts_route(
 
 @router.get('/all', status_code=status.HTTP_200_OK)
 async def get_user_posts(
+    target: Optional[str] = None,
     user: dict[str, str] = Depends(verify_token),
 ):
-    return await get_user_posts_controllers(user["uid"])
+    return await get_user_posts_controllers(user["uid"], target)
